58400e6b230def344c5b8e8523d5dc3cd7499ea5
Chao <chaoyangyes@gmail.com>
2026-08-01 21:16:27 +0800

    feat: update icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cc-gogogo-v19";
+const CACHE = "cc-gogogo-v20";
 const ASSETS = ["./daily-checkin.html", "./style.css", "./app.js", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
