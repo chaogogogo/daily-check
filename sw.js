df4f523c2ca1941573121f6eb625fd89b5a7bf0b
Chao <chaoyangyes@gmail.com>
2026-08-04 21:34:43 +0800

    feat: enhance app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cc-gogogo-v63";
+const CACHE = "cc-gogogo-v64";
 const ASSETS = ["./daily-checkin.html", "./style.css?v=63", "./app.js?v=63", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
